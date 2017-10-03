5460e7f3cce51859490a65fb699230d483caee9e
olga-nest <olga.w.nesterova@gmail.com>
2017-10-03 13:46:06 -0700

    Get user's input, remove new line and white spaces

--- main.m
+++ main.m
@@ -2,7 +2,22 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        char answerCString;
         
+        
+        while (1 > 0) {
+            
+            printf("Your answer:");
+            fgets(&answerCString, 255, stdin);
+            
+            NSString *result = [NSString stringWithCString:&answerCString
+                                                  encoding:NSUTF8StringEncoding];
+            
+            NSCharacterSet *resultSet = [NSCharacterSet whitespaceAndNewlineCharacterSet];
+            NSString *answer = [result stringByTrimmingCharactersInSet:(NSCharacterSet *)resultSet];
+            NSLog(@"Your answer is: %@", answer);
+            
+        }
     }
     return 0;
 }
